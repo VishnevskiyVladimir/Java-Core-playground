1042ba00379de71808c866fb9db79047a8abe30c
Vishnevskiy Vlaimir Home Laptop <vishnevskiy.vladimir.mbox@gmail.com>
2021-02-26 20:44:05 +0300

    Add example of using volatile
    Sometimes we have such output:
    Still false
    Still false
    set true
    Still false
    Still false
    Still false
    
    That is because sometimes threads take value of flag from their caches instead of ram. Because of that they can miss changing of the variable
    
    Volatile defines that variable will not be cached (always will be in ram).

---
+++ src/main/java/multithreading/dmdev/lesson25/vol/VolatileDemo.java
@@ -0,0 +1,41 @@
+package multithreading.dmdev.lesson25.vol;
+
+public class VolatileDemo {
+    private static boolean flag = false;
+    //private static volatile boolean flag = false;
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            while (!flag) {
+                System.out.println("Still false");
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            while (!flag) {
+                System.out.println("Still false");
+            }
+        });
+        Thread thread3 = new Thread(() -> {
+            while (!flag) {
+                System.out.println("Still false");
+            }
+        });
+        Thread thread4 = new Thread(() -> {
+            while (!flag) {
+                System.out.println("Still false");
+            }
+        });
+        Thread thread5 = new Thread(() -> {
+            flag = true;
+            System.out.println("set true");
+
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread5.start();
+
+    }
+}
