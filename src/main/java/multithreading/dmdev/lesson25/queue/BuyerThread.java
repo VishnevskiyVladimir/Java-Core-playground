b3a7068316995579358cffb02045406c84d936e3
Vishnevskiy Vlaimir Home Laptop <vishnevskiy.vladimir.mbox@gmail.com>
2021-02-27 17:52:37 +0300

    Add example of using wait(),notify(),notifyAll()
    Those methods can only be used in synchronized blocks.
    
    wait() - thread releases monitor and pauses until is notified
    wait(timemillis) - thread releases monitor and pauses on specified time
    notify()- notification of a random thread
    notifyAll()- notification of all threads

--- src/main/java/multithreading/dmdev/lesson25/queue/BuyerThread.java
+++ src/main/java/multithreading/dmdev/lesson25/queue/BuyerThread.java
@@ -4,7 +4,7 @@ import java.util.Queue;
 
 public class BuyerThread implements Runnable {
 
-    Queue<CashBox> cashBoxes;
+    final Queue<CashBox> cashBoxes;
 
     public BuyerThread(Queue<CashBox> cashBoxes) {
         this.cashBoxes = cashBoxes;
@@ -13,21 +13,24 @@ public class BuyerThread implements Runnable {
     @Override
     public void run() {
         try {
-            while (true) {
-                if (!cashBoxes.isEmpty()) {
-                    CashBox cashbox = cashBoxes.remove();
-                    System.out.println(Thread.currentThread().getName() + " took  " + cashbox);
-                    Thread.sleep(5L);
+            synchronized (cashBoxes) {
+                while (true) {
+                    if (!cashBoxes.isEmpty()) {
+                        CashBox cashbox = cashBoxes.remove();
+                        System.out.println(Thread.currentThread().getName() + " took  " + cashbox);
+                        cashBoxes.wait(5L);
 
 
-                    System.out.println(Thread.currentThread().getName() + " gave back " + cashbox);
-                    cashBoxes.add(cashbox);
-                    break;
-                } else {
-                    System.out.println(Thread.currentThread().getName() + " is waiting ");
-                    Thread.sleep(5L);
+                        System.out.println(Thread.currentThread().getName() + " gave back " + cashbox);
+                        cashBoxes.add(cashbox);
+                        cashBoxes.notifyAll();
+                        break;
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + " is waiting ");
+                        cashBoxes.wait();
+                    }
+
                 }
-
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
