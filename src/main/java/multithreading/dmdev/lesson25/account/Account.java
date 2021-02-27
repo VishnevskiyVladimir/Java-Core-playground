9cc27b5ed43e659976474b372ebcb488c8c42046
Vishnevskiy Vlaimir Home Laptop <vishnevskiy.vladimir.mbox@gmail.com>
2021-02-27 18:37:27 +0300

    Add example of deadlock
    sometimes program does not finish.
    
    thread1 holds account1 waits releasing of account2.
    thread2 holds account2 waits releasing of account1.

---
+++ src/main/java/multithreading/dmdev/lesson25/account/Account.java
@@ -0,0 +1,36 @@
+package multithreading.dmdev.lesson25.account;
+
+public class Account {
+    private static int generator = 1;
+    private int id;
+    private int money;
+
+    public Account(int money) {
+        this.id = generator++;
+        this.money = money;
+    }
+
+    public void putMoney(int money) {
+        this.money += money;
+    }
+
+    public boolean takeMoney(int money) {
+        if (this.money >= money){
+            this.money -= money;
+            return true;
+        } else {
+            return  false;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "generator=" + generator +
+                ", id=" + id +
+                ", money=" + money +
+                '}';
+    }
+
+}
